7ff934a7b5ae855765c5a6fb5715aecdc867a17b
BlerinaP <blerina1.p@gmail.com>
2019-11-06 10:20:55 +0100

    Creating a sidebar.

--- themes/torontoOnline/index.php
+++ themes/torontoOnline/index.php
@@ -1,1 +1,13 @@
-<h1>Hello from my template.</h1>
+<?php get_header(); ?>
+
+<div id="primary" class="primary">
+    <?php while(have_posts() ): the_post();?>
+
+    <h2><?php the_title(); ?></h2>
+    <?php the_content(); ?>
+        <h2>From index.php</h2>
+    <?php endwhile; ?>
+</div>
+
+<?php get_sidebar(); ?>
+<?php get_footer();?>
---
+++ themes/torontoOnline/sidebar.php
@@ -0,0 +1,5 @@
+<div id="secondary" class="secondary">
+    <h1>Hello from sidebar.php</h1>
+</div>
+
+<div class="clear"></div>
