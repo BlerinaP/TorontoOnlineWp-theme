5f30d11d9e4cfdf2beb6294cd1fa1de54551d474
BlerinaP <blerina1.p@gmail.com>
2019-11-06 10:32:28 +0100

    Adding a Widget Zone.

---
+++ themes/torontoOnline/functions.php
@@ -0,0 +1,28 @@
+<?php
+function torontoOnline_scripts() {
+    wp_enqueue_style('style', get_stylesheet_uri() );
+}
+add_action('wp_enqueue_scripts', 'torontoOnline_scripts' );
+
+
+/*Navigation*/
+register_nav_menus(array(
+   'main_menu' => __('Main Menu','torontoonline')
+));
+
+/*Widget Zone*/
+function theme_widgets(){
+    register_sidebar(array(
+        'name' => __('Sidebar Testimonials'),
+        'id' => 'testimonials',
+        'description' => 'Testimonials Widgets',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>'
+    ));
+}
+add_action('widgets_init', 'theme_widgets');
+
+add_filter('show_admin_bar','__return_false');
+
