dacd4ae4b529f3008017a6297740516589bad774
BlerinaP <blerina1.p@gmail.com>
2019-11-06 11:26:54 +0100

    Checking if a featured image exists and Printing it.

--- themes/torontoOnline/page.php
+++ themes/torontoOnline/page.php
@@ -1,8 +1,15 @@
-<?php get_header(); ?>
-<?php the_post_thumbnail('featured'); ?>
+    <?php get_header(); ?>
+
+    <?php if(has_post_thumbnail() ) { ?>
+        <div class="featured"></div>
+        <?php the_post_thumbnail('featured'); ?>
+        <h2><?php the_title(); ?></h2>
+    <?php } else { ?>
+        <h2 class="noimage"><?php the_title(); ?></h2>
+        <?php } ?>
+
     <div id="primary" class="primary post-<?php the_ID(); ?>">
         <?php while(have_posts() ): the_post();?>
-            <h2><?php the_title(); ?></h2>
             <?php the_content(); ?>
         <?php endwhile; ?>
     </div>
