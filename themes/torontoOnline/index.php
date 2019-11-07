7daf49a7ac7f094d34c953094a90c5fcfabba5dd
BlerinaP <blerina1.p@gmail.com>
2019-11-07 11:27:53 +0100

    Making some changes to blog template file, and styling it with css, also stylings the page for mobile devices.

--- themes/torontoOnline/index.php
+++ themes/torontoOnline/index.php
@@ -1,13 +1,21 @@
 <?php get_header(); ?>
+    <div id="primary" class="primary post-<?php the_ID(); ?>">
+        <?php while(have_posts() ): the_post();?>
 
-<div id="primary" class="primary">
-    <?php while(have_posts() ): the_post();?>
+            <article>
+                <a href="<?php the_permalink();?>">
+                    <?php the_post_thumbnail('visit'); ?>
+                </a>
+                <div class="content-post">
+                    <h2><?php the_title(); ?></h2>
+                   <date class="time"><?php the_time('m/d/Y'); ?></date>
+                    <?php the_category(); ?>
+                    <?php the_excerpt(); ?>
+                </div>
 
-    <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>
-        <h2>From index.php</h2>
-    <?php endwhile; ?>
-</div>
+            </article>
 
+        <?php endwhile; ?>
+    </div>
 <?php get_sidebar(); ?>
 <?php get_footer();?>
